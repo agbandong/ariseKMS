a51d7cb771538555188c381ef09639d563f11aa2
agbandong <69942400+agbandong@users.noreply.github.com>
2022-10-31 22:45:11 +0800

    Added Logos

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,8 @@
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"/>
+    
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
