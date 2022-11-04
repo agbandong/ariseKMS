58d283aabe49adb3c8d28a6916311d9b3b1719e3
agbandong <69942400+agbandong@users.noreply.github.com>
2022-11-05 04:19:20 +0800

    Created Project Controller

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\Project;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,4 +33,5 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
+    Route::resource('/projects', Project::class);
 });
