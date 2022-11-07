f861a6a834907146cec8415a63ee4eaad87ec21d
agbandong <69942400+agbandong@users.noreply.github.com>
2022-11-08 01:28:56 +0800

    Added organization and user migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,12 +16,20 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            /*
+            Remove later when fixing users
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('title');
+            $table->foreignId('organization_id');
+            $table->string('position');*/
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->foreignId('current_project_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
+            
             $table->timestamps();
         });
     }
--- database/migrations/2022_11_03_121229_create_organizations_table.php
+++ database/migrations/2022_11_03_121229_create_organizations_table.php
@@ -15,7 +15,37 @@ return new class extends Migration
     {
         Schema::create('organizations', function (Blueprint $table) {
             $table->id();
+            //Company Information
+            $table->string('name');
+            $table->string('legal_representative_name');
+            $table->string('legal_representative_title');
+            $table->string('company_country');
+            $table->string('headquarters_country');
+            $table->string('legal_company_country');
+            $table->boolean('has_proof');
+            $table->string('registration_proof_path', 2048)->nullable();
+            //TODO address
+            $table->string('website', 2048);
+            $table->boolean('is_trade_org');
+            $table->boolean('is_in_association');
+            $table->string('exchange_name')->nullable();
+            $table->string('exchange_symbol', 10)->nullable();
+            $table->string('is_subsiary_details')->nullable();
+            $table->unsignedInteger('number_employees', 10000000);
+            $table->unsignedInteger('past_annual_revenue');
+            //UN AND CSR RELATED AFFILIATIONS
+            $table->boolean('is_member_UN_compact');
+            $table->string('affiliated_UN_agency')->nullable();
+            //TODO
+            //list any other organizations or platforms in the area of DRR, CSR or sustainable development that you are a member of
+            //list and attach a copy of any documents that showcase your current engagement in DRR, CSR or sustainable development
+            $table->string('sector');
+            //Other
+            $table->string('description_business_and_activities');
+            $table->string('description_business_activities');
             $table->timestamps();
+            //User
+            $table->foreignId('primary_contact_id');
         });
     }
 
