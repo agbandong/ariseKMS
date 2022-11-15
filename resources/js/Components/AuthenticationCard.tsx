0b0b7d6dc65d4950629450154bfa20705173fd3a
agbandong <69942400+agbandong@users.noreply.github.com>
2022-11-16 02:26:49 +0800

    Edited Login

--- resources/js/Components/AuthenticationCard.tsx
+++ resources/js/Components/AuthenticationCard.tsx
@@ -9,7 +9,6 @@ export default function AuthenticationCard({
       <div>
         <AuthenticationCardLogo />
       </div>
-
       <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
         {children}
       </div>
