dd324e6545939e7a3af2e6b300193e62135e4e49
agbandong <69942400+agbandong@users.noreply.github.com>
2022-11-16 13:28:19 +0800

    Cleared everything

--- resources/js/Pages/Example.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import useRoute from '@/Hooks/useRoute';
-
-export default function Example(){
-    const route = useRoute;
-    return(
-        <h1>Enter html here</h1>
-    )
-}
