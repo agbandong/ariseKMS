55a423df86d9e655341bc2160bd0096baddeee07
agbandong <69942400+agbandong@users.noreply.github.com>
2022-11-19 12:03:14 +0800

    Edited Login to be responsive

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ApplicationLogo from '@/Components/ApplicationLogo';
 import { Link } from '@inertiajs/inertia-react';
 
-export default function Guest({ children }) {
+export default function Guest({ title='', titleStyle='', children }) {
     return (
         <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div className='mt-4'>
@@ -10,6 +10,7 @@ export default function Guest({ children }) {
                     <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
                 </Link>
             </div>
+            <div className='mt-4 block lg:hidden'><h1 className={`text-2xl ${titleStyle}`}>{title}</h1></div>
 
             <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 {children}
--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -33,32 +33,32 @@ export default function Login({ status, canResetPassword }) {
     };
     //HTML page
     return (
-        
-        <div className="px-4 py-5 px-md-5 text-center text-lg-start" style={{backgroundColor: 'hsl(0, 0%, 96%)'}}>
-            {/* Header */}
-            <div className='grid grid-cols-2'>
-                {/*Left side*/}
-                {/* <div className='hidden lg:block'>
-                    <h1>Welcome to Arise PH</h1>
-                    <p>This is the Knowledge Management System designed for the resiliency projects of Arise PH</p>
-                </div> */}
-                <container className="col-lg-6 mb-5 mb-lg-0" style={{margin: '30px'}}>
-                    <div style={{
-                        position: 'absolute', left: '25%', top: '50%',
-                        transform: 'translate(-50%, -50%)'
-                    }}>
-                    <h3 className="my-5 display-3 fw-bold ls-tight px-3" style={{fontSize: '30px'}}>
-                    Welcome to&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<br/><br/>ARISE-Philippines</h3>
-                    <h1 className="my-5 display-3 fw-bold ls-tight px-3 bolded" style={{fontSize: '30px', color: '#2d3b9c'}}>Knowledge Managemenet Portal</h1>
+        <div className='grid grid-cols-2 px-4 py-5 px-md-5 text-center text-lg-start' style={{backgroundColor: 'hsl(0, 0%, 96%)'}}>
 
-                    <p style={{color: 'hsl(217, 10%, 50.8%)', paddingTop: '20px'}}>
-                    This is the Knowledge Management System<br/> designed for the resiliency projects of ARISE-Philippines.
-                    </p>
-                    </div>
-                </container>
+            {/*Left side*/}
+            {/* <div className='hidden lg:block'>
+                <h1>Welcome to Arise PH</h1>
+                <p>This is the Knowledge Management System designed for the resiliency projects of Arise PH</p>
+            </div> */}
+            <div className="hidden lg:block col-lg-6 mb-5 mb-lg-0" style={{margin: '30px'}}>
+                <div style={{
+                    position: 'absolute', left: '25%', top: '50%',
+                    transform: 'translate(-50%, -50%)'
+                }}>
+                <h3 className="my-5 display-3 fw-bold ls-tight px-3" style={{fontSize: '30px'}}>
+                Welcome to&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<br/><br/>ARISE-Philippines</h3>
+                <h1 className="my-5 display-3 fw-bold ls-tight px-3 bolded" style={{fontSize: '30px', color: '#2d3b9c'}}>Knowledge Management Portal</h1>
 
-                {/*Right side*/}
-                <GuestLayout className="col-span-2 lg:col-span-1">
+                <p style={{color: 'hsl(217, 10%, 50.8%)', paddingTop: '20px'}}>
+                This is the Knowledge Management System<br/> designed for the resiliency projects of ARISE-Philippines.
+                </p>
+                </div>
+            </div>
+            
+            
+            {/*Right side*/}
+            <div className="col-span-2 lg:col-span-1">
+                <GuestLayout title='Arise-Philippines Knowledge Management Portal'>
                     <Head title="Log in" />
 
                     {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
@@ -117,18 +117,20 @@ export default function Login({ status, canResetPassword }) {
                             </PrimaryButton>
                             
                         </div>
+                        <div className='float-right block'>
+                            <Link href={route('register/organization')}>
+                            <SecondaryButton className="my-2 ml-4 float-right">
+                                Register Organization
+                            </SecondaryButton>
+                            </Link>
+                            <p className='float-right'>Organization already registered?</p>
+                            <Link href={route('register')}>
+                                <SecondaryButton className="my-2 ml-4 float-right">
+                                    Register as User
+                                </SecondaryButton>
+                            </Link>
+                        </div>
                     </form>
-                    <Link href={route('register/organization')}>
-                    <SecondaryButton className="my-2 ml-4 float-right">
-                        Register Organization
-                    </SecondaryButton>
-                    </Link>
-                    <p className='float-right'>Organization already registered?</p>
-                    <Link href={route('register')}>
-                        <SecondaryButton className="my-2 ml-4 float-right">
-                            Register as User
-                        </SecondaryButton>
-                    </Link>
                 </GuestLayout>
             </div>
         </div>
