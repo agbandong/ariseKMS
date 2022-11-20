0f76a47685f3091927aba2b99461a34fa6cc6b6b
agbandong <69942400+agbandong@users.noreply.github.com>
2022-11-20 12:10:23 +0800

    Added flatpickr

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 const plugin = require('tailwindcss/plugin');
 
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
@@ -66,7 +67,6 @@ module.exports = {
 
     plugins: [
         require('@tailwindcss/forms'),
-        
         plugin(({ addVariant, e }) => {
             addVariant('sidebar-expanded', ({ modifySelectors, separator }) => {
               modifySelectors(({ className }) => `.sidebar-expanded .${e(`sidebar-expanded${separator}${className}`)}`);
