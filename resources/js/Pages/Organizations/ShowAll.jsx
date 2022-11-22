d262b61bbbca036b9e78dab540b45a14af8f3400
Erika Shiokawa <eshiokawa@student.apc.edu.ph>
2022-11-22 21:32:57 +0900

    Added counter on dashboard

--- resources/js/Pages/Organizations/ShowAll.jsx
+++ resources/js/Pages/Organizations/ShowAll.jsx
@@ -15,7 +15,7 @@ export default function ShowAll(props) {
         </h2>
       }
         head="Organizations"
-    >        
+    >   
         <div className="py-12">
           <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div className='float-right mb-4'>
